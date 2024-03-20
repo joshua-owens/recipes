42253134ca29880c2fa9afd34508beb07ab9cf51
Josh Owens <me@jowens.dev>
2024-03-19 21:48:41 -0500

    echo webserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, Chef!")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
