9df1606612ef5158e6cb3b39d317abcb2f4affea
Josh Owens <me@jowens.dev>
2024-04-22 20:39:55 -0500

    python grpc server

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 - [docker](https://www.docker.com/)
 - [goose](https://github.com/pressly/goose)
 - [sqlc](https://github.com/sqlc-dev/sqlc)
+
+## Code Generation
+
+- Generating the python gRPC server code
+
+```shell
+python3 -m grpc_tools.protoc -I protos/ --python_out=cmd/scraper/ --grpc_python_out=cmd/scraper protos/recipe.proto
+```
--- cmd/scraper/index.py
+++ cmd/scraper/index.py
@@ -1,5 +1,24 @@
+import grpc
+from concurrent import futures
 from recipe_scrapers import scrape_me
+import recipe_pb2 
+import recipe_pb2_grpc
 
-recipe = scrape_me("https://www.budgetbytes.com/slow-cooker-chicken-tikka-masala/", wild_mode=True)
+class RecipeServiceServicer(recipe_pb2_grpc.RecipeServiceServicer):
+    def GetRecipe(self, request, context):
+        recipe = scrape_me(request.url, wild_mode=True)
+        return recipe_pb2.RecipeResponse(
+            title=recipe.title(),
+            ingredients='\n'.join(recipe.ingredients()),
+            instructions=recipe.instructions()
+        )
 
-print(recipe.to_json())
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    recipe_pb2_grpc.add_RecipeServiceServicer_to_server(RecipeServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
