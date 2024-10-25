4ff1141568138c1d976408895f7d3cb9afb43afc
Josh Owens <me@jowens.dev>
2024-10-24 20:09:19 -0500

    feat(scraper): Post request to scrape a given url

--- scraper/main.py
+++ scraper/main.py
@@ -1,10 +1,25 @@
 from typing import Union
+import requests
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from recipe_scrapers import scrape_html
 
 app = FastAPI()
 
+class RecipeRequest(BaseModel):
+    url: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/recipe")
+def create_recipe(request: RecipeRequest):
+    html = requests.get(request.url, headers={"User-Agent": "Recipe Seeker"}).content
+    scraper = scrape_html(html, org_url=request.url)
+    return {
+        "image": scraper.image(),
+        "ingredients": scraper.ingredients(),
+        "instructions": scraper.instructions_list(),
+        "nutrients": scraper.nutrients(),
+        "title": scraper.title(),
+        "total_time": scraper.total_time(),
+        "yields": scraper.yields(),
+    }
